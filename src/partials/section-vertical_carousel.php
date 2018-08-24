7a48c48fcda966cd9ec69638137d57fd11c679cd
Jon Senterfitt <thetwosents@gmail.com>
2018-08-24 00:18:04 -0500

    Update verticals

--- src/partials/section-vertical_carousel.php
+++ src/partials/section-vertical_carousel.php
@@ -12,7 +12,7 @@ $vertical_title = '';
 <section class="vertical-carousel">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-md-8">
+      <div class="col-xs-12 col-md-8 col-lg-9">
         <div class="box" data-aos="fade-right" data-aos-anchor-placement="center-bottom">
           <!-- <h4><?php //echo $above_title; ?></h4> -->
           <h1><?php echo $title; ?></h1>
