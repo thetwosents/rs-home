7e6a5ccb9d3c6db0c51b83fe893bdd29836061cb
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-08-26 19:13:02 -0500

    Press timeline added

---
+++ src/partials/section-press_timeline.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<section class="press-timeline">
+	<div class="container">
+		<div class="row middle-xs center-xs">
+			<div class="col-xs-1">
+				<img src="http://placehold.it/48x48">
+			</div>
+			<div class="col-xs-10">
+				<div class="box">
+					<div class="row middle-xs start-xs">
+						<div class="col-xs-3">
+							<img src="http://placehold.it/400x500">
+						</div>
+						<div class="col-xs-9">
+							<div class="content">
+								<h2>#rsTheCon</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus pulvinar diam ultricies porttitor. </p>
+							</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+			<div class="col-xs-1">
+				<img src="http://placehold.it/48x48">
+			</div>
+		</div>
+	</div>
+</section>
