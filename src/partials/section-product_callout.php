0c1eee97d2c203af2a337a413b52b889317fdfd2
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-08-29 23:04:40 -0500

    Update z-index position

--- src/partials/section-product_callout.php
+++ src/partials/section-product_callout.php
@@ -18,9 +18,8 @@ $have_secondary_cta = get_sub_field('have_secondary_cta'); // true / false
 $secondary_cta_text = get_sub_field('secondary_cta_text');
 $secondary_cta_link = get_sub_field('secondary_cta_link');
 
-
-
 $top_photo = get_sub_field('top_photo');
+
 $left_photo_x = get_sub_field('left_photo_x');
 $left_photo_y = get_sub_field('left_photo_y');
 
@@ -33,12 +32,11 @@ $right_photo_y = get_sub_field('right_photo_y');
   <div class="container">
     <div class="row middle-xs">
 
-  <?php if ($left_or_right_side === 'left') { ?>
-    
+    <?php if ($left_or_right_side === 'left') { ?>
       <div class="col-xs-12 col-md-8 col-lg-7">
         <?php if (!$product_photos): ?>
           <?php if( $images ): ?>
-              <div class="images">
+              <?php echo '<div class="images ' . $top_photo . '">' ?>
                 <div class="background jarallax" style="margin-top: <?php echo $left_photo_y; ?>px; margin-left: <?php echo $left_photo_x; ?>px;" data-jarallax data-disable-parallax="/iPad|iPhone|iPod|Android/" data-speed="0.96">
                   <img class="jarallax-img" data-disable-parallax="/iPad|iPhone|iPod|Android/" src="<?php echo wp_get_attachment_image_url( $images[0]['ID'], 'full' ); ?>" alt="">
                 </div>
@@ -88,7 +86,8 @@ $right_photo_y = get_sub_field('right_photo_y');
       <div class="col-xs-12 col-md-8 col-lg-8 right-aligned first-xs last-md">
         <?php if (!$product_photos): ?>
           <?php if( $images ): ?>
-              <div class="images">
+            <?php echo '<div class="images ' . $top_photo . '">' ?>
+              
                 <div class="background jarallax" style="margin-top: <?php echo $left_photo_y; ?>px; margin-left: <?php echo $left_photo_x; ?>px;" data-disable-parallax="/iPad|iPhone|iPod|Android/" data-jarallax data-speed="0.96">
                   <img class="jarallax-img" src="<?php echo wp_get_attachment_image_url( $images[0]['ID'], 'full' ); ?>" alt="">
                 </div>
