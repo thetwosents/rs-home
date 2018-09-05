01787342b37926254fb25d3c32e12f8a94109f89
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-05 09:22:43 -0500

    Update

--- src/page.php
+++ src/page.php
@@ -4,9 +4,18 @@ get_header(); ?>
 
 <main class="main" role="main">
 <?php // open the WordPress loop
-if (have_posts()) : while (have_posts()) : the_post();
-the_content();
-endwhile; endif; // close the WordPress loop ?>
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+  <section class="page">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-md-12">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php endwhile; endif; // close the WordPress loop ?>
 </main>
 
 <?php get_footer(); ?>
