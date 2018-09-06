dac18e7dc18b6c55697f850ba0f0a59b4b9dbd11
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-06 16:31:31 -0500

    Update

--- src/partials/section-image_callout.php
+++ src/partials/section-image_callout.php
@@ -4,7 +4,7 @@ $image = get_sub_field('image_block');
 
 ?>
 
-<section class="image-callout">
+<section class="image-callout light--spacing">
   <div class="container">
     <!-- <div class="row center-xs">
       <div class="col-xs-12 col-md-10">
