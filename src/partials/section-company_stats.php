faa5b979ef3ac349666804acc502b401cdddb9c0
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-09 13:15:06 -0500

    Update

--- src/partials/section-company_stats.php
+++ src/partials/section-company_stats.php
@@ -4,57 +4,49 @@
 
 <section class="company-stats">
 	<div class="container">
-		<div class="row top-xs center-xs">
+		<div class="row top-xs start-xs">
+
+			<?php 
+
+			if( have_rows('steps') ): while ( have_rows('steps') ) : the_row();
+
+
+				$title = get_sub_field('title');
+				$content = get_sub_field('content');
+
+				$stat_1_number = get_sub_field('stat_1_number');
+				$stat_1_text = get_sub_field('stat_1_text');
+				$stat_2_number = get_sub_field('stat_2_number');
+				$stat_2_text = get_sub_field('stat_2_text');
+
+				$cta_text = get_sub_field('cta_text');
+				$cta_link = get_sub_field('cta_link');
+
+			?>
+			
+
 			<div class="col-xs-12 col-md-4">
 				<div class="box">
 					<div class="content">
-						<h2>Brands</h2>
-						<p>Brands are the paint to the rewardStyle influencer’s brush. Items are meticulously selected and organically promoted through the platforms that consumers are already utilizing for daily lifestyle and shopping inspiration. Brands are provided end-to-end strategic guidance and in-depth data to design campaigns tailored to generate maximum impact.</p>
-						<!-- <div class="stat">
-							<h1>4,500+</h1>
-							<h4>Retailers</h4>
+						<h2><?php echo $title; ?></h2>
+						<?php echo $content; ?>
+						<div class="stat">
+							<h1><?php echo $stat_1_number; ?></h1>
+							<h4><?php echo $stat_1_text; ?></h4>
 						</div>
 						<div class="stat">
-							<h1>1M</h1>
-							<h4>Brands</h4>
-						</div> -->
+							<h1><?php echo $stat_2_number; ?></h1>
+							<h4><?php echo $stat_2_text; ?></h4>
+						</div>
+						<a href="<?php echo $cta_link; ?>" class="btn"><?php echo $cta_text; ?></a>
 					</div>
 				</div>	
 			</div>
-			<div class="col-xs-12 col-md-4">
-				<div class="box">
-					<div class="content">
-						<h2>Influencers</h2>
-						<p>From day one, retail influencers have sat at the center of the rewardStyle mission due to their ability to authentically inspire their audiences through curated messaging.They define retail trends by connecting purchase-ready consumers with the brands and products they’re searching for.</p>
-						<!-- <div class="stat">
-							<h1>4,500+</h1>
-							<h4>Retailers</h4>
-						</div>
-						<div class="stat">
-							<h1>1M</h1>
-							<h4>Brands</h4>
-						</div> -->
-					</div>
-				</div>	
-			</div>
-			<div class="col-xs-12 col-md-4">
-				<div class="box last">
-					<div class="content">
-						<h2>Consumers</h2>
-						<p>rewardStyle has forever changed consumers’ online shopping experience: the stronger the relationships between influencers and brands, the more curated the consumer’s content experience becomes. Our influencer ecosystem benefits more than just retailers. It provides a smoother and more efficient purchasing process for buyers across the world.</p>
-						<!-- <div class="stat">
-							<h1>4,500+</h1>
-							<h4>Retailers</h4>
-						</div>
-						<div class="stat">
-							<h1>1M</h1>
-							<h4>Brands</h4>
-						</div> -->
-					</div>
-				</div>	
-			</div>
+
+			<?php endwhile; else : endif; ?>
+			
 		</div>
-		<div class="row center-xs top-xs">
+		<!-- <div class="row center-xs top-xs">
 			<div class="col-xs-12 col-md-4">
 				<div class="box">
 					<div class="stat">
@@ -92,6 +84,6 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 	</div>
 </section>
