a9037fe9d7e25e4b2370d04a71086fcc3bb5917a
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-10 12:13:41 -0500

    Bottom text on image

--- src/partials/section-image_callout.php
+++ src/partials/section-image_callout.php
@@ -4,6 +4,7 @@
 $title = get_sub_field('title');
 $subheading = get_sub_field('subheading');
 $image = get_sub_field('image_block'); 
+$bottom_text = get_sub_field('bottom_text');
 
 ?>
 
@@ -23,6 +24,13 @@ $image = get_sub_field('image_block');
           <img src="<?php echo $image; ?>">
         </div>
       </div>  
+    </div>
+    <div class="row center-xs">
+      <div class="col-xs-12 col-md-10">
+        <div class="box">
+          <h4><?php echo $bottom_text; ?></h4>
+        </div>
+      </div>
     </div>
   </div>
 </section>
