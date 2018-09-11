5f75b5c47d37ddcebd14d9ee8f6581763cd7a699
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-11 10:50:13 -0500

    Smaller space for awards

--- src/partials/section-job_postings.php
+++ src/partials/section-job_postings.php
@@ -5,12 +5,12 @@ $image = get_sub_field('map_image');
 <section class="job-postings">
   <div class="container">
     <div class="row center-xs">
-    	<div class="col-xs-12 col-md-6">
+    	<div class="col-xs-12 col-md-12">
     		<div class="box">
-    			<img src="<?php echo $image; ?>">
+    			<img src="<?php echo $image; ?>" style="margin-bottom: 4vw;">
     		</div>
     	</div>
-      <div class="col-xs-12 col-md-6">
+      <div class="col-xs-12 col-md-12">
       	<div class="box">
         	<div id="BambooHR"><script src="https://rewardstyle.bamboohr.com/js/jobs2.php" type="text/javascript"></script><div id="BambooHR-Footer">Powered by<a href="http://www.bamboohr.com" target="_blank" rel="noopener external nofollow noreferrer"><img src="https://resources.bamboohr.com/images/footer-logo.png" alt="BambooHR - HR software"/></a></div></div> 
         </div>
