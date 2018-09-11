4136d6929e04b22b72c2a144c30490fb24ac87cd
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-10 22:46:16 -0500

    Update

--- src/partials/section-mission_statement.php
+++ src/partials/section-mission_statement.php
@@ -12,7 +12,7 @@ $isLarge = get_sub_field('is_large');
       <div class="col-xs-12 col-md-10">
         <div class="box" 
           data-aos="fade-up" 
-          data-aos-anchor-placement="center-bottom" 
+          data-aos-anchor-placement="top-bottom" 
           data-aos-duration="1200">
           <h2><?php echo $header; ?></h2>
           <?php if ($isLarge) { ?>
