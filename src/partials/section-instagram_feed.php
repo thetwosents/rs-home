919eeb3b3690200ed2b180cf4ce46c8afd13c371
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-09-11 19:15:27 -0500

    Update

--- src/partials/section-instagram_feed.php
+++ src/partials/section-instagram_feed.php
@@ -8,7 +8,7 @@
 				<div class="box">
 					<h1>rewardStyle Family</h1>
 					<h2>Our culture embraces our differences. Each of us is a unique and valued contributor to all that makes up #rStheFamily.</h2>
-					<a href="" class="btn">View Positions</a>
+					<a href="https://www.instagram.com/rewardstylehq/?hl=en" target="_blank" class="btn">Follow Us <i class="fab fa-instagram"></i></a>
 				</div>
 			</div>
 		</div>
